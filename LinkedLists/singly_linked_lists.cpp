06e0573a72d2df6d49cbe488bea75030aa1c4bc4
marslanali <marslanali800@gmail.com>
2022-01-16 21:34:27 +0500

    minor changes

--- LinkedLists/singly_linked_lists.cpp
+++ LinkedLists/singly_linked_lists.cpp
@@ -48,50 +48,63 @@ int main()
   linked_list_traversal(head);
 
   // Insert a node at the beginning
-  Node* new_node_beginning = new Node();
-  new_node_beginning->data = 5;
-  new_node_beginning->next = first;
-  head = new_node_beginning;
-  // traverse the updated linked list
-  head = new_node_beginning;
-  linked_list_traversal(head);
+  {
+    Node* new_node_beginning = new Node();
+    new_node_beginning->data = 5;
+    new_node_beginning->next = first;
+    head = new_node_beginning;
+    // traverse the updated linked list
+    head = new_node_beginning;
+    linked_list_traversal(head);
+  }
 
   // Insert a node at the end
-  Node* new_node_end = new Node();
-  new_node_end->data = 40;
-  third->next = new_node_end;
-  new_node_end->next = NULL;
-  // traverse the upated linked list
-  linked_list_traversal(head);
+  {
+    Node* new_node_end = new Node();
+    new_node_end->data = 40;
+    third->next = new_node_end;
+    new_node_end->next = NULL;
+    // traverse the upated linked list
+    linked_list_traversal(head);
+  }
 
   // Insert a node at the middle
-  Node* new_node_middle = new Node();
-  second->next = new_node_middle;
-  new_node_middle->next = third;
-  // traverse the upated linked list
-  linked_list_traversal(head);
+  {
+    Node* new_node_middle = new Node();
+    second->next = new_node_middle;
+    new_node_middle->next = third;
+    // traverse the upated linked list
+    linked_list_traversal(head);
+  }
 
   // delete from the beginning
-  head = head->next;
-  // traverse the updated linked list
-  linked_list_traversal(head);
+  {
+    Node* tmp = new Node();
+    tmp = head;
+    head = head->next;
+    free(tmp);
+    // traverse the updated linked list
+    linked_list_traversal(head);
+  }
 
   // delete node from the end
-  // create two tmp pointer holding head
-  Node* tmp1 = head;
-  Node* tmp2 = head;
-  // traverse with the tmp pointer to reach second last node
-  while (tmp1->next != NULL)
   {
-    tmp2 = tmp1;
-    tmp1 = tmp1->next;
+    // create two tmp pointer holding head
+    Node* tmp1 = head;
+    Node* tmp2 = head;
+    // traverse with the tmp pointer to reach second last node
+    while (tmp1->next != NULL)
+    {
+      tmp2 = tmp1;
+      tmp1 = tmp1->next;
+    }
+    // delete the last node
+    tmp2->next = NULL;
+    free(tmp1);
+    tmp1 = NULL;
+    // traverse the updated linked list
+    linked_list_traversal(head);
   }
-  // delete the last node
-  tmp2->next = NULL;
-  free(tmp1);
-  tmp1 = NULL;
-  // traverse the updated linked list
-  linked_list_traversal(head);
 
   return 0;
 }
